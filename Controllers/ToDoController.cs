16691f1325aeb410b8d2dd1eefaffb0971cdf3de
chomax21 <maximcheb21@gmail.com>
2023-02-04 21:41:32 +0300

    Создание ToDoController

--- Controllers/ToDoController.cs
+++ Controllers/ToDoController.cs
@@ -1,9 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Angular_2.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Angular_2.Controllers
 {
-    public class ToDoController : ControllerBase
+    [ApiController]
+    public class ToDoController : ControllerBase     
     {
+        private readonly ApplicationContext _context;
 
+        public ToDoController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public IResult GetToDoList(int id)
+        {
+            var toDo = _context.ToDoLists.FirstOrDefault(x=> x.Id == id);
+            return null;
+        }
     }
 }
