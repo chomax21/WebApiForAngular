3c787fc1a4ec6cdee459af7ad46285e1aa263b49
MaxChe <Max@HOME-PC>
2023-03-08 17:49:07 +0300

    + свойство приоритета.

--- Models/ToDoList.cs
+++ Models/ToDoList.cs
@@ -6,5 +6,15 @@
         public string Case { get; set; }
         public DateTimeOffset DateTime { get; set; } = DateTimeOffset.Now;
         public bool IsDone { get; set; } = false;
+        public int Priority
+        {
+            get { return Priority; }
+            set 
+            {
+                if (value < 0 && value > 2)
+                    throw new ArgumentException("Приоритет должен иметь значения от 0 до 2");
+                Priority = value;
+            }
+        }
     }
 }
